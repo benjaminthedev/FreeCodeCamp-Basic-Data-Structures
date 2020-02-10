85efddc36883b5ae063ccfc08c1c79fe47dd6e01
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-02-10 07:45:56 +0000

    updated lesson on splice

---
+++ lesson05-remove-items-using-splice.js
@@ -0,0 +1,9 @@
+//remove-items-using-splice
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// only change code below this line
+
+const newArr = arr.splice(1, 4);
+
+// only change code above this line
+console.log(arr);
